b6d2c5b4be402faf36d4f47e3267aa7425b9d12b
decemberster <decemberster@bk.ru>
2016-10-22 21:42:10 +1000

    test

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 const debug = require('debug')('sql-strip-comments');
 
-// Удаление комментариев
-// Комментарии могут быть однострочными: от "--" и до конца строки,
-// или многострочными: от "/*" до "*/".
-// Символы комментариев внутри строковых констрант не учитываются.
-// Вложенные комментарии не поддерживаются
-// https://larrysteinle.com/2011/02/09/use-regular-expressions-to-clean-sql-statements/
+// strip comments in sql
+// one line comments: from "--" to end of line,
+// or multiline: from "/*" to "*/".
+// string literals with sql comments omited
+// nested comments are not supported
+// inspired: https://larrysteinle.com/2011/02/09/use-regular-expressions-to-clean-sql-statements/
 
 function removeComments(sql) {
     sql = sql.replace(/("(""|[^"])*")|('(''|[^'])*')|(--[^\n\r]*)|(\/\*[\w\W]*?(?=\*\/)\*\/)/gm, (match) => {
-            if (
-        (match[0] === '"' && match[match.length-1] === '"')
-        || (match[0] === "'" && match[match.length-1] === "'")
-) return match;
+        if (
+            (match[0] === '"' && match[match.length - 1] === '"')
+            || (match[0] === "'" && match[match.length - 1] === "'")
+        ) return match;
 
-    debug('comment removed: {\n%s\n}', match);
-    return '';
-});
+        debug('comment removed: {\n%s\n}', match);
+        return '';
+    });
 
     return sql;
 }
@@ -26,27 +26,27 @@ function removeComments(sql) {
 function minifySQL(sql) {
     // Удалить все табуляции и переносы строк
     sql = sql.replace(/("(""|[^"])*")|('(''|[^'])*')|([\t\r\n])/gm, (match) => {
-            if (
-        (match[0] === '"' && match[match.length-1] === '"')
-        || (match[0] === "'" && match[match.length-1] === "'")
-) return match;
+        if (
+            (match[0] === '"' && match[match.length - 1] === '"')
+            || (match[0] === "'" && match[match.length - 1] === "'")
+        ) return match;
 
-    return ' ';
-});
+        return ' ';
+    });
 
     // Сократить все дублирующиеся пробелы
     sql = sql.replace(/("(""|[^"])*")|('(''|[^'])*')|([ ]{2,})/gm, (match) => {
-            if (
-        (match[0] === '"' && match[match.length-1] === '"')
-        || (match[0] === "'" && match[match.length-1] === "'")
-) return match;
+        if (
+            (match[0] === '"' && match[match.length - 1] === '"')
+            || (match[0] === "'" && match[match.length - 1] === "'")
+        ) return match;
 
-    return ' ';
-});
+        return ' ';
+    });
 
     return sql.trim();
 }
 
-module.exports = function(sql) {
-    return minifySQL( removeComments(sql) )
+module.exports = function (sql) {
+    return minifySQL(removeComments(sql))
 };
